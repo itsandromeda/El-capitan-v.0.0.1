20805fc009679f8671812ab79fc1b9eef8f68f87
itsandromeda <itsandromeda@gmail.com>
2017-03-07 17:14:25 -0500

    Coders update

--- Assets/js/student-files.js
+++ Assets/js/student-files.js
@@ -1,58 +1,61 @@
-var profiles = [ {
-        nombre : "Meche",
-        apellido : "Zubieta",
-        promocion : "1era. promoción",
-        estado : "Trabajando"
-    }, {
-        nombre : "Arabela",
-        apellido : "Rojas",
-        promocion : "2era. promoción",
-        estado : "Trabajando"
-    }, {
-        nombre : "María",
-        apellido : "Rosán",
-        promocion : "2era. promoción",
-        estado : "Trabajando"
-    }, {
-        nombre : "Guadalupe",
-        apellido : "Racio",
-        promocion : "3era. promoción",
-        estado : "Trabajando"
-    }, {
-        nombre : "Sara",
-        apellido : "Casa",
-        promocion : "3era. promoción",
-        estado : "Trabajando"
-    }, {
-        nombre : "Daniela",
-        apellido : "Durán",
-        promocion : "4ta. promoción",
-        estado : "Trabajando"
-    }, {
-        nombre : "Michelle",
-        apellido : "Seguil",
-        promocion : "4ta. promoción",
-        estado : "Trabajando"
-    }, {
-        nombre : "Diana",
-        apellido : "Navarro",
-        promocion : "5ta. promoción",
-        estado : "Trabajando"
-    } ],
-    perfil,
-    i,
-    p = document.getElementById("students");
-
-function print() {
+function showList() {
     "use strict";
-    var perfiles = "";
-    for (i = 0; i < profiles.length; i += 1) {
-        perfiles += "<div style='float:left;background-color:#F8A819'>" + "<ul style='list-style: none'>" +
-                "<li> Nombre: " + profiles[i].nombre + "</li>" +
-                "<li> Apellido: " + profiles[i].apellido + "</li>" +
-                "<li> Promoción: " + profiles[i].promocion + "</li>" +
-                "<li> Estado: " + profiles[i].estado + "</li><br></ul>" + "</div>";
-    }
-    p.innerHTML += perfiles;
+    var profiles = [ {
+            nombre : "Meche",
+            apellido : "Zubieta",
+            promocion : "1era. promoción",
+            estado : "Trabajando"
+        }, {
+            nombre : "Arabela",
+            apellido : "Rojas",
+            promocion : "2era. promoción",
+            estado : "Trabajando"
+        }, {
+            nombre : "María",
+            apellido : "Rosán",
+            promocion : "2era. promoción",
+            estado : "Trabajando"
+        }, {
+            nombre : "Guadalupe",
+            apellido : "Racio",
+            promocion : "3era. promoción",
+            estado : "Trabajando"
+        }, {
+            nombre : "Sara",
+            apellido : "Casa",
+            promocion : "3era. promoción",
+            estado : "Trabajando"
+        }, {
+            nombre : "Daniela",
+            apellido : "Durán",
+            promocion : "4ta. promoción",
+            estado : "Trabajando"
+        }, {
+            nombre : "Michelle",
+            apellido : "Seguil",
+            promocion : "4ta. promoción",
+            estado : "Trabajando"
+        }, {
+            nombre : "Diana",
+            apellido : "Navarro",
+            promocion : "5ta. promoción",
+            estado : "Trabajando"
+        } ],
+        list = "",
+        p = document.getElementById("students");
+    
+    profiles.forEach(function (e) {
+        list += "<div class = 'students'>" +
+                    "<ul style='list-style: none'>" +
+                        "<li> Nombre: " + e.nombre + "</li>" +
+                        "<li>Apellido: " + e.apellido + "</li>" +
+                        "<li>Promoción: " + e.promocion + "</li>" +
+                        "<li>Estado: " + e.estado + "</li>" +
+                    "</ul>" +
+                "</div>";
+    });
+    
+    p.innerHTML = list;
 }
-print();
+
+showList();
